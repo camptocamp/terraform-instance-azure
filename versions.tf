b7592956f47c52166b76c7b59ef2a6a136b4f179
Mickaël Canévet <mickael.canevet@camptocamp.com>
2020-12-16 14:31:20 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,3 +1,18 @@
 terraform {
-  required_version = ">= 0.12"
+  required_providers {
+    null = {
+      source = "hashicorp/null"
+    }
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
+    random = {
+      source = "hashicorp/random"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
+
+  required_version = ">= 0.13"
 }
